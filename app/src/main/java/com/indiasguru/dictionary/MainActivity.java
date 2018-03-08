9169a586e60a89cce1d667c5e4a02c18ec681985
bharathnv <bharathnadignv4@@gmail.com>
2018-03-08 08:07:34 +0530

    Splash sreeen funtionality

--- app/src/main/java/com/indiasguru/dictionary/MainActivity.java
+++ app/src/main/java/com/indiasguru/dictionary/MainActivity.java
@@ -1,13 +1,24 @@
 package com.indiasguru.dictionary;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN_TIME_OUT = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, SampleWords.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
